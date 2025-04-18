f0331dc3ce4e869ce3770d880a49c9e69fc68014
hugop <h.perez10@usp.ceu.es>
2025-04-18 17:32:49 +0000

    Shipment Class

--- logistics_api.py
+++ logistics_api.py
@@ -1,8 +1,17 @@
 # Language: Python 3.13.2
 
 from enum import Enum
+from dataclasses import dataclass
 
 class Status(Enum):
     PENDING= "Pending"
     IN_TRANSIT= "InTransit"
     DELIVERED= "Delivered"    
+
+@dataclass
+class Shipment():
+    """Internal data storage class for each shipment"""
+    tracking_id: str
+    destination: str
+    status: Status
+    weight: float
